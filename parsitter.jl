7cf2240bc06211b624e90137b493700a05eb262a
Corneliu Cofaru <cornel@oxoaresearch.com>
2026-02-17 15:21:52 +0100

    Add runic formatting

--- parsitter.jl
+++ parsitter.jl
@@ -12,30 +12,29 @@ using JSON
 
 # Function that parses Garamond's unix-socket client arguments
 function get_arguments(args::Vector{String})
-	s = ArgParseSettings()
-	@add_arg_table! s begin
+    s = ArgParseSettings()
+    @add_arg_table! s begin
         "input"
-            help = "entitity to parse (directory, file or snippet of code"
-            arg_type = String
+        help = "entitity to parse (directory, file or snippet of code"
+        arg_type = String
         "--input-type"
-            help = "What is being parsed. Available 'file', 'directory', 'code'"
-            arg_type = String
+        help = "What is being parsed. Available 'file', 'directory', 'code'"
+        arg_type = String
         "--language"
-            help = "Programming language. Available: 'python', 'julia', 'c', 'c#', 'r'"
-            arg_type = String
+        help = "Programming language. Available: 'python', 'julia', 'c', 'c#', 'r'"
+        arg_type = String
         "--print-code"
-            help = "Whether to print out the code that is parsed"
-            action = :store_true
+        help = "Whether to print out the code that is parsed"
+        action = :store_true
         "--escape-chars"
-            help = "Whether to parse \\n, \\t etc. as escape chars. Active only for 'code' inputs"
-            action = :store_true
+        help = "Whether to parse \\n, \\t etc. as escape chars. Active only for 'code' inputs"
+        action = :store_true
         "--log-level"
-            help = "logging level"
-            default = "error"
-	end
-	return parse_args(args,s)
+        help = "logging level"
+        default = "error"
+    end
+    return parse_args(args, s)
 end
-
 
 
 ########################
@@ -57,20 +56,22 @@ function real_main()
     args = get_arguments(ARGS)
 
     # Logging
-    log_levels = Dict("debug" => Logging.Debug,
-                      "info" => Logging.Info,
-                      "warning" => Logging.Warn,
-                      "error" => Logging.Error)
+    log_levels = Dict(
+        "debug" => Logging.Debug,
+        "info" => Logging.Info,
+        "warning" => Logging.Warn,
+        "error" => Logging.Error
+    )
 
     log_level = get(log_levels, lowercase(args["log-level"]), Logging.Error)
-    logger = ConsoleLogger(stdout,log_level)
+    logger = ConsoleLogger(stdout, log_level)
     global_logger(logger)
 
     ###
     input = args["input"]
-    input_type= args["input-type"]
+    input_type = args["input-type"]
     language = args["language"]
-    escape_chars= args["escape-chars"]
+    escape_chars = args["escape-chars"]
     print_code = args["print-code"]
     ###
     ParSitter.check_tree_sitter()
@@ -102,7 +103,7 @@ end
 
 main_script_file = abspath(PROGRAM_FILE)
 
-if occursin("debugger",main_script_file) || main_script_file == @__FILE__
+if occursin("debugger", main_script_file) || main_script_file == @__FILE__
     real_main()
 end
 
